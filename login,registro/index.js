4e3480678acc85d18431b1e14cef139ab4b4d227
nicole <nicolmarquezok@gmail.com>
2023-05-11 13:37:25 -0300

    agregue unos cambios

--- login,registro/index.js
+++ login,registro/index.js
@@ -1,12 +1,12 @@
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
-    window.location.href = 'login.html'
+    window.location.href = '/login,registro/paginadeadm.html'
 }
 
-const logout = document.querySelector('#logout')
+const logout = document.querySelector('#admin')
 
 logout.addEventListener('click', ()=>{
     alert('Hasta pronto!')
     localStorage.removeItem('login_success')
-    window.location.href = 'login.html'
+    window.location.href = '/login,registro/paginadeadm.html'
 })
